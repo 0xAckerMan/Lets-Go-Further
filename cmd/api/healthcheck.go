1a6d27dcfd6c26519b6b905908f59319b41aa4fb
0xAckerMan <joelkores1@gmail.com>
2023-10-26 22:09:42 +0300

    Server healthcheck

---
+++ cmd/api/healthcheck.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (app *Application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
+	status := envelope{
+		"system_info": map[string]string{
+			"Status":      "Active",
+			"Version":     Version,
+			"Environment": app.config.env,
+		},
+	}
+
+	err := app.writeJson(w, http.StatusOK, status, nil)
+	if err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	}
+}
