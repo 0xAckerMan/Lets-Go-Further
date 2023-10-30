0302e6c90427740be7e45365f12fb8f9576ada41
0xAckerMan <joelkores1@gmail.com>
2023-10-30 22:31:09 +0300

    Quick fix

---
+++ cmd/api/movies.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/0xAckerMan/Lets-Go-Further/internal/data"
+)
+
+func (app *Application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Create a new Book")
+}
+
+func (app *Application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		http.NotFound(w, r)
+		return
+	}
+
+	movie := &data.Movie{
+		Id:        id,
+		CreatedAt: time.Now(),
+		Title:     "Click Click Bang",
+		Runtime:   180,
+		Genres:    []string{"drama", "romance", "crime"},
+		Version:   1,
+	}
+
+	err = app.writeJson(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "The server encounter a problem, cannot process your request", http.StatusInternalServerError)
+	}
+}
