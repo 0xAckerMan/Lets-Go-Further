850d977c1ca594fa92338d9cdf501801e0636839
0xAckerMan <joelkores1@gmail.com>
2023-11-05 21:37:14 +0300

    Handling errors helper functions

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (app *Application) logError (r *http.Request, err error){
     app.logger.Println(err)
@@ -8,4 +11,26 @@ func (app *Application) logError (r *http.Request, err error){
 
 func (app *Application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}){
     env := envelope{"error": message}
+    err := app.writeJson(w, http.StatusOK, env, nil)
+    if err != nil{
+        app.logError(r, err)
+        w.WriteHeader(500)
+    }
+}
+
+func (app*Application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error){
+    app.logError(r,err)
+    message :="Sorry the server experienced an error, we cant complete your request at the moment"
+    app.errorResponse(w, r, http.StatusInternalServerError, message)
+}
+
+func (app *Application) notFoundResponse(w http.ResponseWriter, r *http.Request){
+    message := "the requested resource could not be found"
+    app.errorResponse(w, r, http.StatusNotFound, message)
+}
+
+func (app *Application) methodNotAllowedResponse(w http.ResponseWriter, r *http.Request){
+    message := fmt.Sprintf("the %s method is not supported for this resource", r.Method)
+
+    app.errorResponse(w, r, http.StatusMethodNotAllowed, message)
 }
