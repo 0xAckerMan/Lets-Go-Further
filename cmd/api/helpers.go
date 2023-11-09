4061923a182309efe1298f0e89d739a9e6af9586
0xAckerMan <joelkores1@gmail.com>
2023-11-09 23:01:32 +0300

    Managing bad requests

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -44,7 +45,13 @@ func (app *Application) writeJson(w http.ResponseWriter, status int, data envelo
 }
 
 func (app *Application) readJson(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	// Use http.MaxBytesReader() to limit the size of the request body to 1MB.
+	maxByte := 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, int64(maxByte))
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var invalidUnmarshalError *json.InvalidUnmarshalError
@@ -60,13 +67,31 @@ func (app *Application) readJson(w http.ResponseWriter, r *http.Request, dst int
 				return fmt.Errorf("body contains incorrect json type for  field %q", unmarshalTypeError.Field)
 			}
 			return fmt.Errorf("body contains incorrect json type(at character %d)", unmarshalTypeError.Offset)
+			// If the request body exceeds 1MB in size the decode will now fail with the
+			// error "http: request body too large".
+		case err.Error() == "http: request body too large":
+			return fmt.Errorf("body must not be larger than %d bytes", maxByte)
+
 		case errors.Is(err, io.EOF):
 			return errors.New("body must not be empty")
+
+		// If the JSON contains a field which cannot be mapped to the target destination
+		case strings.HasPrefix(err.Error(), "json: unknown field"):
+			fieldName := strings.TrimPrefix(err.Error(), "json: unknown field")
+			return fmt.Errorf("body contains unknown key %s", fieldName)
 		case errors.As(err, &invalidUnmarshalError):
 			panic(err)
 		default:
 			return err
 		}
 	}
-    return nil
+    err = dec.Decode(&struct{}{})
+    if err != io.EOF{
+        return errors.New("body must only contain one json value")
+    }
+	return nil
+}
+
+func (app *Application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	app.errorResponse(w, r, http.StatusBadRequest, err.Error())
 }
